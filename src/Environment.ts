b9a694ccdd797d5a0eb59c2520f7d7c30a3eb6ed
GrimmAdone <yyxl0913@gmail.com>
2020-12-21 11:22:54 +0800

    修改配置

--- src/Environment.ts
+++ src/Environment.ts
@@ -1,6 +1,7 @@
 const config = {
   'development': {
-    'BASE_API': 'https://ics.scgecloud.com:28028'
+    //'BASE_API': 'https://ics.scgecloud.com:28028'
+    'BASE_API': 'http://ics.scgecloud.com'
   },
   'production': {
     'BASE_API': 'https://ics.scg.cn'
